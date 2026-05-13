f47a21c00da9b2f3e91af8d3d14fd1911831e5ba
Shota0112 <shotaku0112@gmail.com>
2026-05-13 11:08:32 +0900

    Add sensor, microphone, and GPS features

--- app/src/main/java/jp/ac/gifu_u/shota/MainActivity.java
+++ app/src/main/java/jp/ac/gifu_u/shota/MainActivity.java
@@ -1,15 +1,69 @@
 package jp.ac.gifu_u.shota;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    private LocationManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_main);
-        setContentView(new MyView(this));
+        setContentView(R.layout.activity_main);
+        manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        }
+        try {
+            if (manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                manager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER, 1000, 10, this);
+            }
+            if (manager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+                manager.requestLocationUpdates(
+                        LocationManager.NETWORK_PROVIDER, 1000, 10, this);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        TextView textview = findViewById(R.id.status_text);
+        textview.setText("位置情報を取得中...");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        manager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        String str = String.format("緯度: %.3f  経度: %.3f", lat, lng);
+        TextView textview = findViewById(R.id.status_text);
+        textview.setText(str);
+        Toast.makeText(this,
+                String.format("%.3f %.3f", lat, lng),
+                Toast.LENGTH_SHORT).show();
     }
 }
